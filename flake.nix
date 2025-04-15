4f8ec525abc9ecf4f5a61c6f77f770a66606807b
patriot720 <cerkin-3@yandex.ru>
2025-04-15 21:51:32 +0300

    fix flake & add .dir-locals & envrc

--- flake.nix
+++ flake.nix
@@ -7,167 +7,167 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     libtgvoip = {
-      url = "git+file:./Telegram/ThirdParty/libtgvoip";
+      url = "https://github.com/telegramdesktop/libtgvoip";
       flake = false;
     };
 
     GSL = {
-      url = "git+file:./Telegram/ThirdParty/GSL";
+      url = "https://github.com/Microsoft/GSL.git";
       flake = false;
     };
 
     xxHash = {
-      url = "git+file:./Telegram/ThirdParty/xxHash";
+      url = "https://github.com/Cyan4973/xxHash.git";
       flake = false;
     };
 
     rlottie = {
-      url = "git+file:./Telegram/ThirdParty/rlottie";
+      url = "https://github.com/desktop-app/rlottie.git";
       flake = false;
     };
 
     lz4 = {
-      url = "git+file:./Telegram/ThirdParty/lz4";
+      url = "https://github.com/lz4/lz4.git";
       flake = false;
     };
 
     lib_crl = {
-      url = "git+file:./Telegram/lib_crl";
+      url = "https://github.com/desktop-app/lib_crl.git";
       flake = false;
     };
 
     lib_rpl = {
-      url = "git+file:./Telegram/lib_rpl";
+      url = "https://github.com/desktop-app/lib_rpl.git";
       flake = false;
     };
 
     lib_base = {
-      url = "git+file:./Telegram/lib_base";
+      url = "https://github.com/desktop-app/lib_base.git";
       flake = false;
     };
 
     codegen = {
-      url = "git+file:./Telegram/codegen";
+      url = "https://github.com/desktop-app/codegen.git";
       flake = false;
     };
 
     lib_ui = {
-      url = "git+file:./Telegram/lib_ui";
+      url = "https://github.com/desktop-app/lib_ui.git";
       flake = false;
     };
 
     lib_lottie = {
-      url = "git+file:./Telegram/lib_lottie";
+      url = "https://github.com/desktop-app/lib_lottie.git";
       flake = false;
     };
 
     lib_tl = {
-      url = "git+file:./Telegram/lib_tl";
+      url = "https://github.com/desktop-app/lib_tl.git";
       flake = false;
     };
 
     lib_spellcheck = {
-      url = "git+file:./Telegram/lib_spellcheck";
+      url = "https://github.com/desktop-app/lib_spellcheck";
       flake = false;
     };
 
     lib_storage = {
-      url = "git+file:./Telegram/lib_storage";
+      url = "https://github.com/desktop-app/lib_storage.git";
       flake = false;
     };
 
     cmake = {
-      url = "git+file:./cmake";
+      url = "https://github.com/desktop-app/cmake_helpers.git";
       flake = false;
     };
 
     expected = {
-      url = "git+file:./Telegram/ThirdParty/expected";
+      url = "https://github.com/TartanLlama/expected";
       flake = false;
     };
 
     QR = {
-      url = "git+file:./Telegram/ThirdParty/QR";
+      url = "https://github.com/nayuki/QR-Code-generator";
       flake = false;
     };
 
     lib_qr = {
-      url = "git+file:./Telegram/lib_qr";
+      url = "https://github.com/desktop-app/lib_qr.git";
       flake = false;
     };
 
     hunspell = {
-      url = "git+file:./Telegram/ThirdParty/hunspell";
+      url = "https://github.com/hunspell/hunspell";
       flake = false;
     };
 
-    range_v3 = {
-      url = "git+file:./Telegram/ThirdParty/range-v3";
+    range-v3 = {
+      url = "https://github.com/ericniebler/range-v3.git";
       flake = false;
     };
 
     nimf = {
-      url = "git+file:./Telegram/ThirdParty/nimf";
+      url = "https://github.com/hamonikr/nimf.git";
       flake = false;
     };
 
     hime = {
-      url = "git+file:./Telegram/ThirdParty/hime";
+      url = "https://github.com/hime-ime/hime.git";
       flake = false;
     };
 
-    fcitx5_qt = {
-      url = "git+file:./Telegram/ThirdParty/fcitx5-qt";
+    fcitx5-qt = {
+      url = "https://github.com/fcitx/fcitx5-qt.git";
       flake = false;
     };
 
     lib_webrtc = {
-      url = "git+file:./Telegram/lib_webrtc";
+      url = "https://github.com/desktop-app/lib_webrtc.git";
       flake = false;
     };
 
     tgcalls = {
-      url = "git+file:./Telegram/ThirdParty/tgcalls";
+      url = "https://github.com/TelegramMessenger/tgcalls.git";
       flake = false;
     };
 
     lib_webview = {
-      url = "git+file:./Telegram/lib_webview";
+      url = "https://github.com/desktop-app/lib_webview.git";
       flake = false;
     };
 
     jemalloc = {
-      url = "git+file:./Telegram/ThirdParty/jemalloc";
+      url = "https://github.com/jemalloc/jemalloc";
       flake = false;
     };
 
     dispatch = {
-      url = "git+file:./Telegram/ThirdParty/dispatch";
+      url = "https://github.com/apple/swift-corelibs-libdispatch";
       flake = false;
     };
 
     kimageformats = {
-      url = "git+file:./Telegram/ThirdParty/kimageformats";
+      url = "https://github.com/KDE/kimageformats.git";
       flake = false;
     };
 
     kcoreaddons = {
-      url = "git+file:./Telegram/ThirdParty/kcoreaddons";
+      url = "https://github.com/KDE/kcoreaddons.git";
       flake = false;
     };
 
     cld3 = {
-      url = "git+file:./Telegram/ThirdParty/cld3";
+      url = "https://github.com/google/cld3.git";
       flake = false;
     };
 
     libprisma = {
-      url = "git+file:./Telegram/ThirdParty/libprisma";
+      url = "https://github.com/desktop-app/libprisma.git";
       flake = false;
     };
 
-    xdg_desktop_portal = {
-      url = "git+file:./Telegram/ThirdParty/xdg-desktop-portal";
+    xdg-desktop-portal = {
+      url = "https://github.com/flatpak/xdg-desktop-portal.git";
       flake = false;
     };
   };
