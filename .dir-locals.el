9b689781c5debacfc2f11be754bbb058fd0fa584
patriot720 <cerkin-3@yandex.ru>
2025-05-17 12:12:44 +0300

    update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,12 @@
       . (progn
           (setq lsp-disabled-clients '(c++-mode clangd))
           (require 'dap-gdb)
+          (require 'dap-cpptools)
+          (setopt dap-internal-terminal #'dap-internal-terminal-shell)
           (dap-register-debug-provider "gdb" 'dap-gdb--populate-gdb)
-          (setq dap-gdb-debug-program `("gdb" "-ex" ,(s-concat "set substitute-path /build/source/ " (projectile-project-root)) "-i" "dap"))
+          (setq dap-gdb-debug-program `("gdb" "-ex" ,(s-concat "set substitute-path /build/source/ " (projectile-project-root))
+                                        "-iex" "set debug dap-log-file /tmp/dap-log.txt"
+                                        "-i" "dap"))
           ;; (setq dap-gdb-debug-program '("rust-gdb" "-i" "dap"))
           (dap-register-debug-template
            "GDB::Run"
@@ -18,6 +22,22 @@
             ;; :cwd (s-concat (projectile-project-root) "Telegram/SourceFiles/")
             :target nil
             ))
+          (dap-register-debug-template
+           "cpptools"
+           (list :type "cppdbg"
+                 :request "launch"
+                 :name "cpptools::Run Configuration"
+                 :stopAtEntry :json-false
+                 :MIMode "gdb"
+                 :program "telegram-desktop"
+                 ;; :filterStdout :json-false
+                 :debuggerPath "stdenv exec . gdb"
+                 ;; :setupCommands (vector
+                 ;;                 (list :text "-enable-pretty-printing"
+                 ;;                       :description "enable pretty-printing for gdb"
+                 ;;                       :ignoreFailures t))
+                 :cwd (projectile-project-root)))
+
           ;; (dap-register-debug-template
           ;;  "GDBServer::Connect"
           ;;  (list :type "gdbserver"
