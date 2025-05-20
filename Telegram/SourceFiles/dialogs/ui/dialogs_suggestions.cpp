b6b56e921d96ea8a84c819ae549e668ad3fda463
patriot720 <cerkin-3@yandex.ru>
2025-05-20 11:55:51 +0300

    remove suggestions and global search

--- Telegram/SourceFiles/dialogs/ui/dialogs_suggestions.cpp
+++ Telegram/SourceFiles/dialogs/ui/dialogs_suggestions.cpp
@@ -1348,10 +1348,10 @@ Suggestions::Suggestions(
 , _recentApps(setupRecentApps())
 , _popularApps(setupPopularApps())
 , _searchQueryTimer([=] { applySearchQuery(); }) {
-	setupTabs();
-	setupChats();
-	setupChannels();
-	setupApps();
+	// setupTabs();
+	// setupChats();
+	// setupChannels();
+	// setupApps();
 }
 
 Suggestions::~Suggestions() = default;
