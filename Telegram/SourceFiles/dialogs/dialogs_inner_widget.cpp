532f5aa75cae501242e97affb822eb7fc7eeaad2
patriot720 <cerkin-3@yandex.ru>
2025-06-10 09:56:49 +0300

    reverse inner widget stuff

--- Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -1162,9 +1162,6 @@ void InnerWidget::paintEvent(QPaintEvent *e) {
 							result->sponsored->data.randomId);
 					}
 					const auto peer = result->peer;
-                    if(peer->isChannel()) {
-                        return;
-                    };
 					const auto active = !activeEntry.fullId
 						&& activePeer
 						&& ((peer == activePeer)
